6dcf3a433c2b48e8e7d99e36182cdda20d6bbe34
idkjay <jordanchu95@gmail.com>
2022-08-04 15:11:58 -0400

    initial commit w first migration and model

---
+++ app/views/entries/_entry.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! entry, :id, :meal_type, :calories, :proteins, :carbohydrates, :fats, :sodium, :created_at, :updated_at
+json.url entry_url(entry, format: :json)
---
+++ db/migrate/20220804190522_create_entries.rb
@@ -0,0 +1,14 @@
+class CreateEntries < ActiveRecord::Migration[7.0]
+  def change
+    create_table :entries do |t|
+      t.string :meal_type
+      t.integer :calories
+      t.integer :proteins
+      t.integer :carbohydrates
+      t.integer :fats
+      t.integer :sodium
+
+      t.timestamps
+    end
+  end
+end
