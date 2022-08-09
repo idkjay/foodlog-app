d32b6e66ead23039ea993ff803f968d448f6c8bc
idkjay <jordanchu95@gmail.com>
2022-08-09 17:36:52 -0400

    archives controller generated

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -60,7 +60,6 @@ class EntriesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_entry
-
       begin
         @entry = Entry.find(params[:id])
       rescue
